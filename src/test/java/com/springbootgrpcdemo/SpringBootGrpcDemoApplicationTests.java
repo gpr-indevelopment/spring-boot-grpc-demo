da0b28323ad8a4b7a4010e5f2585139450176b1d
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-10-04 20:27:12 -0300

    Spring initializer

---
+++ src/test/java/com/springbootgrpcdemo/SpringBootGrpcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootgrpcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGrpcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
